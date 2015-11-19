ffadd1e7fcb2e62c138565d1cb5c61d18a25c52e
Agata Cieplik <ac@novelpay.pl>
2015-11-20 00:51:58 +0100

    Wydarzenia z eventful #5
    closes #5
    
    Obsluga przydatnych funkcji z api eventful + plik z konfiguracja

---
+++ app/xmlToJson.py
@@ -0,0 +1,28 @@
+import simplejson as json
+from xml.dom import minidom
+import urllib2
+
+def parse_element(element):
+    dict_data = dict()
+    if element.nodeType == element.TEXT_NODE:
+        dict_data['data'] = element.data
+    if element.nodeType not in [element.TEXT_NODE, element.DOCUMENT_NODE,
+                                element.DOCUMENT_TYPE_NODE]:
+        for item in element.attributes.items():
+            dict_data[item[0]] = item[1]
+    if element.nodeType not in [element.TEXT_NODE, element.DOCUMENT_TYPE_NODE]:
+        for child in element.childNodes:
+            child_name, child_dict = parse_element(child)
+            if child_name in dict_data:
+                try:
+                    dict_data[child_name].append(child_dict)
+                except AttributeError:
+                    dict_data[child_name] = [dict_data[child_name], child_dict]
+            else:
+                dict_data[child_name] = child_dict
+    return element.nodeName, dict_data
+
+def xmlToJson(url):
+    result = urllib2.urlopen(url).read()
+    dom = minidom.parseString(result)
+    return json.loads(json.dumps(parse_element(dom), sort_keys=True, indent=4))
