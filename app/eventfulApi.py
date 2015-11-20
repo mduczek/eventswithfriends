1c74fa5a65da7fbc4b1ab3840bc242faa563f250
Agata Cieplik <ac@novelpay.pl>
2015-11-20 03:23:17 +0100

    xc

--- app/eventfulApi.py
+++ app/eventfulApi.py
@@ -22,13 +22,13 @@ Supported filters:
     category - limits to categories returned by getCategories()
     others: check http://api.eventful.com/docs/events/search
 """
-#@app.route("/eventfulApi/filterEvents", methods=["GET"])
-#def filterEvents(filterDictionary):
-    #searchString = ""
-    #for (key, value) in filterDictionary.items():
-        #searchString += "&" + key + "=" + value
-    #url = DOMAIN+"/events/search?app_key="+API_KEY+searchString
-    #return xmlToJson(url)
+@app.route("/eventfulApi/filterEvents", methods=["GET"])
+def filterEvents(filterDictionary):
+    searchString = ""
+    for (key, value) in filterDictionary.items():
+        searchString += "&" + key + "=" + value
+    url = DOMAIN+"/events/search?app_key="+API_KEY+searchString
+    return xmlToJson(url)
 
 
 """ Returns the list of all categories """
@@ -39,9 +39,9 @@ def getCategories():
 
 
 """ Filters all performers, either keywords parameter or category is required """
-#@app.route("/eventfulApi/filterPerformers/<string:keywords>", methods=["GET"])
-#@app.route("/eventfulApi/filterPerformers/<string:category>", methods=["GET"])
-#@app.route("/eventfulApi/filterPerformers/<string:keywords>/<string:category>", methods=["GET"])
+@app.route("/eventfulApi/filterPerformers/<string:keywords>", methods=["GET"])
+@app.route("/eventfulApi/filterPerformers/<string:category>", methods=["GET"])
+@app.route("/eventfulApi/filterPerformers/<string:keywords>/<string:category>", methods=["GET"])
 def filterPerformers(keywords=None, category=None):
     searchString = ""
     if (keywords != None):
