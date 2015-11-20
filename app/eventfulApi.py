66955e5d6a1a652ede9786f6fa64c605c3667dac
Agata Cieplik <ac@novelpay.pl>
2015-11-20 03:05:04 +0100

    api test

--- app/eventfulApi.py
+++ app/eventfulApi.py
@@ -1,3 +1,4 @@
+from app import app
 from xmlToJson import xmlToJson
 import ConfigParser
 
@@ -19,6 +20,7 @@ Supported filters:
     category - limits to categories returned by getCategories()
     others: check http://api.eventful.com/docs/events/search
 """
+@app.route("/eventfulApi/filterEvents", methods["GET"])
 def filterEvents(filterDictionary):
     searchString = ""
     for (key, value) in filterDictionary.items():
@@ -28,12 +30,16 @@ def filterEvents(filterDictionary):
 
 
 """ Returns the list of all categories """
+@app.route("/eventfulApi/getCategories", methods["GET"])
 def getCategories():
     url = DOMAIN + "/categories/list?app_key=" + API_KEY
     return xmlToJson(url)
 
 
 """ Filters all performers, either keywords parameter or category is required """
+@app.route("/eventfulApi/filterPerformers/<string:keywords>", methods["GET"])
+@app.route("/eventfulApi/filterPerformers/<string:category>", methods["GET"])
+@app.route("/eventfulApi/filterPerformers/<string:keywords>/<string:category>", methods["GET"])
 def filterPerformers(keywords=None, category=None):
     searchString = ""
     if (keywords != None):
@@ -48,6 +54,7 @@ def filterPerformers(keywords=None, category=None):
 Returns the list of events for particular performer
 Id might be obtained by calling filterPerformers first
 """
+@app.route("/eventfulApi/performerEvents/<string:performerId>", methods["GET"])
 def performerEvents(performerId):
     searchString = "&id=" + performerId
     url = DOMAIN + "/performers/events/list?app_key=" + API_KEY + searchString
