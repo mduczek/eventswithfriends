7c7eea3159878ed1a8755ad3b2ff87cc272c2334
Agata Cieplik <ac@novelpay.pl>
2015-11-20 09:42:55 +0100

    eventful + interests!

--- app/static/js/events.js
+++ app/static/js/events.js
@@ -1,18 +1,83 @@
-function events_main() {
-    log("events_main");
+var eventful_events_locations = [];
+var eventful_events_interests_city = [];
+var eventful_events_interests_country = [];
+var interests = [];
+var city, country;
+var x;
 
-    eventful_events = [];
+function fetch_interests(json) {
+    if (json._source.interests.music) {
+        interests = $.merge(interests, json._source.interests.music);
+    }
+    if (json._source.interests.movies) {
+        interests = $.merge(interests, json._source.interests.movies);
+    }
+    if (json._source.interests.television) {
+        interests = $.merge(interests, json._source.interests.television);
+    }
+    if (json._source.interests.games) {
+        interests = $.merge(interests, json._source.interests.games);
+    }
+}
+
+function filter_location(json, callback) {
+    var obj = new Object();
+    fetch_interests(json);
+    if (json._source.location) {
+        var tmp = json._source.location.split(",");
+        city = tmp[0];
+        country = tmp[1];
+        obj.location = json._source.location;
+        filterEvents(obj, function (d) {
+            if (d[1].search.events.event) {
+                var events = d[1].search.events.event;
+                for (var e in events) {
+                    for (var i in interests) {
+                        if (events[e].description && events[e].description["#text"] && events[e].description["#text"].data.indexOf(interests[i]) > -1 || 
+                            events[e].title && events[e].title["#text"] && events[e].title["#text"].data.indexOf(interests[i]) > -1) {
+                            eventful_events_locations.push(events[e]);
+                            break;
+                        }
+                    }
+                }
+                if (callback)
+                    callback(json);
+            }
+        });
+    }
+}
+
+function filter_musicalEvents(json) {
+    var musicians = json._source.interests.music;
+    if (musicians) {
+        for (var perf in musicians) {
+            filterPerformers("", musicians[perf], function (d) {
+                if (d[1].search.performers.performer && d[1].search.performers.performer.id) {
+                    var id = d[1].search.performers.performer.id["#text"].data;
+                    performerEvents(id, function (dd) {
+                        if (dd[1].events.event) {
+                            var events = dd[1].events.event;
+                            for (var e in events) {
+                                if (events[e].city && events[e].city["#text"].data === city) {
+                                    eventful_events_interests_city.push(events[e]);
+                                }
+                                if (events[e].country && events[e].country["#text"].data === country) {
+                                    eventful_events_interests_country.push(events[e]);
+                                }
+                            }
+                        }
+                    });
+                }
+            });
+        }
+    }
+}
+
+function events_main() {
 
     es_get_id("preferences", FB.getUserID(), function(json_data) {
         json = JSON.parse(json_data);
-        log(json._source);
-        var obj = new Object();
-        obj.location = json._source.location;
-        filterEvents(obj, function (d) {
-            log(d[1].search.events.event);
-            eventful_events = $.merge(eventful_events, d[1].search.events.event);
-            log(eventful_events);
-        });
+        filter_location(json, filter_musicalEvents );
     });
 }
 
@@ -97,7 +162,6 @@ function putUserFriendsEvents() {
                     ev['friend_of_user'] = user_id;
                     ev['friend_id'] = friend;
                     es_put_id('events', user_id + '_' + friend + '_' + ev.id, ev, empty);
-                    //console.log('events', user_id + '_' + friend + '_' + ev.id, ev);
                 }
             }
         }
@@ -105,7 +169,6 @@ function putUserFriendsEvents() {
 }
 
 function pageAll(resp, data, callback) {
-    console.log('GOT:', resp);
     data.push.apply(data, resp.data);
     next = resp.paging.next;
     if (next) {
