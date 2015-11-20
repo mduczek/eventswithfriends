a9fb5e0dc61fcf07e68eef4df767b5138ca50118
Michal Duczynski <mduczek@gmail.com>
2015-11-19 17:57:24 -0800

    put in es

--- app/views.py
+++ app/views.py
@@ -19,3 +19,28 @@ def events():
 @app.route('/error', methods=['GET', 'POST'])
 def error():
     return render_template('error.html')
+
+#md ========
+import requests
+import json
+
+DB = "http://paas:bc9998c29d76573ab6b7196952e5490d@dwalin-us-east-1.searchly.com/esdb"
+
+@app.rout('/put_events/<user>', methods=['POST'])
+def put_events(user):
+    events = json.loads(request.data)
+    resp = ""
+    for event in events['my']:
+        event['user'] = user
+        event_id = event['id']
+        r = requests.put(DB+'/events/'+user+'_'+event_id, data=json.dumps(event))
+        resp += r.text
+    for event in events['of_friends']:
+        event['of_friend'] = user
+        event_id = event['id']
+        r = requests.put(DB+'/events'+user+'_'+event_id, data=json.dumps(event))
+        resp += r.text
+
+    return resp
+
+
