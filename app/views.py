c503458684100adcaeeab38c8d8194b041efd983
Michal Duczynski <mduczek@gmail.com>
2015-11-19 18:47:14 -0800

    fix

--- app/views.py
+++ app/views.py
@@ -26,7 +26,7 @@ import json
 
 DB = "http://paas:bc9998c29d76573ab6b7196952e5490d@dwalin-us-east-1.searchly.com/esdb"
 
-@app.route('/es_search', methods=['GET']):
+@app.route('/es_search', methods=['GET'])
     return 'searching'
 
 @app.route('/es_put', methods=['POST'])
